948b13313fcdf15d510d2d75a9871556babd91b2
Behrad Niazmand <bniazmand@Behrads-MacBook-Pro.local>
2016-04-25 11:33:30 +0300

    Scripts updated

---
+++ scripts/Data_collection_scripts/data_collection_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+[[ -d ../collected_results ]] || mkdir ../collected_results
+
+for i in ../Results/* ; do
+  if [ -d "$i" ]; then
+  		cp latency_script.sh $i
+  		cp energy_script.sh $i
+  		cp throughput_script.sh $i
+  		cd $i
+  		echo moved to ${PWD##*/} folder
+      if [ -d results ]; then
+          echo removed results folder!
+          rm -r results
+      fi
+      echo running the scripts
+  		sh latency_script.sh && sh energy_script.sh && sh throughput_script.sh
+      echo copying results to collected_results folder
+  		cp results/* ../../collected_results
+      echo cleaning up...
+  		rm -r results
+  		rm latency_script.sh && rm energy_script.sh && rm throughput_script.sh
+  		cd ../../data_collection_scripts
+      echo -----------------
+  fi
+done
---
+++ scripts/main_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sh noxim_script.sh
+sh data_collection_script.sh
